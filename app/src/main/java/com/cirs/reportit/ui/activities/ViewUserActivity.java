bc1f5a5263a58d37163b7ba5cdd8c4db9c3de7fc
Kshitij <kpg257@gmail.com>
2016-03-03 23:48:28 +0530

    Added ViewUserActivity (blank)

---
+++ app/src/main/java/com/cirs/reportit/ui/activities/ViewUserActivity.java
@@ -0,0 +1,15 @@
+package com.cirs.reportit.ui.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.cirs.R;
+
+public class ViewUserActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_user);
+    }
+}
