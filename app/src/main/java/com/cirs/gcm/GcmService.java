23569e90a9fe4edcd73291408fc666e1dd573699
Rohan <rkamat30@gmail.com>
2016-03-03 06:40:08 +0530

    Add backstack to pendingintent of notification

--- app/src/main/java/com/cirs/gcm/GcmService.java
+++ app/src/main/java/com/cirs/gcm/GcmService.java
@@ -3,6 +3,7 @@ package com.cirs.gcm;
 import android.app.PendingIntent;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.TaskStackBuilder;
 import android.util.Log;
 
 import com.cirs.reportit.ui.activities.ViewComplaintActivity;
@@ -33,8 +34,15 @@ public class GcmService extends GcmListenerService {
         Log.d(TAG, "From: " + from);
         Log.d(TAG, "compId: " + compId);
         Intent i = new Intent(this, ViewComplaintActivity.class);
-        i.putExtra(ViewComplaintActivity.EXTRA_COMPLAINT_ID, compId);
-        PendingIntent pi = PendingIntent.getActivity(this, 100, i, PendingIntent.FLAG_ONE_SHOT);
+		i.putExtra(ViewComplaintActivity.EXTRA_COMPLAINT_ID, compId);
+		
+		TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+		// Adds the back stack
+		stackBuilder.addParentStack(ViewComplaintActivity.class);
+		// Adds the Intent to the top of the stack
+		stackBuilder.addNextIntent(i);
+		// Gets a PendingIntent containing the entire back stack
+		PendingIntent pi = stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
         NotificationHelper.showNotification(this, "ReportIt", "The status of your complaint was changed to " + status, pi);
 
     }
